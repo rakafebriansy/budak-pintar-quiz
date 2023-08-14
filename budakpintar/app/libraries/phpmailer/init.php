df1e6de68cb302648b74a5b2e98746e4cc90b363
rakafebriansy <kerenr445@gmail.com>
2023-08-14 11:04:47 +0700

    add quiz optimize genre button

--- budakpintar/app/libraries/phpmailer/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require_once 'src/Exception.php';
-require_once 'src/PHPMailer.php';
-require_once 'src/SMTP.php';
-?>
