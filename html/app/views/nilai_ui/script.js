3e0455d5eab7a712e2d71257e560d4c2fbb225ba
Juniyasyos <juniyasyos@gmail.com>
2023-08-18 23:40:12 +0700

    perbaikan animasi

--- html/app/views/nilai_ui/script.js
+++ html/app/views/nilai_ui/script.js
@@ -6,11 +6,23 @@ const starTwo = document.getElementById("two");
 const starThree = document.getElementById("three");
 const starFour = document.getElementById("four");
 
+let confettiAnimationInterval;
+function stopConfetti() {
+  const confettiContainer = document.querySelector(".confetti-container");
+  confettiContainer.innerHTML = "";
+}
+
 function createConfetti() {
   const confettiContainer = document.querySelector(".confetti-container");
   const confettiElement = document.createElement("div");
   confettiElement.classList.add("confetti");
   confettiContainer.appendChild(confettiElement);
+
+  // Set the animation duration and delay dynamically
+  const animationDuration = Math.random() * 3 + 2; // Random duration between 2 and 5 seconds
+  const animationDelay = Math.random() * 2; // Random delay between 0 and 2 seconds
+  confettiElement.style.animationDuration = `${animationDuration}s`;
+  confettiElement.style.animationDelay = `${animationDelay}s`;
 }
 
 function animateScore(targetScore) {
@@ -44,12 +56,16 @@ submitBtn.addEventListener("click", function () {
   for (let i = 0; i < 10; i++) {
     createConfetti();
   }
-
-  // Ngubah Nilainya sesuai database ya raka :)
   const targetScore = 100;
   animateScore(targetScore);
 
   resultPopup.style.display = "block";
+
+  // Start the interval for stopping the confetti animation after 5 seconds
+  confettiAnimationInterval = setTimeout(() => {
+    stopConfetti();
+    clearInterval(confettiAnimationInterval); 
+  }, 10000);
 });
 
 closeBtn.addEventListener("click", function () {
