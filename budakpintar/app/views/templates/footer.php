59589ab36e1879e4bf09df616283dd4f8b1cab43
rkfbrn <kerenr445@gmail.com>
2023-09-22 19:17:50 +0700

    new

--- budakpintar/app/views/templates/footer.php
+++ budakpintar/app/views/templates/footer.php
@@ -1,24 +1,11 @@
 <footer class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary mt-5 container-fluid">
-    <!-- Copyright -->
-    <div class="text-white mb-3 mb-md-0">
-        Copyright © 2020. All rights reserved.
-    </div>
-    <!-- Copyright -->
 
     <!-- Right -->
-    <div>
-        <a href="#!" class="text-white me-4">
-            <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-            <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-            <i class="fab fa-google"></i>
-        </a>
-        <a href="#!" class="text-white">
-            <i class="fab fa-linkedin-in"></i>
-        </a>
+    <div class="d-flex align-items-center text-white">
+        <img src="<?=BASEURL;?>/img/instagram.png" style="margin-right:1em" width="20" alt="">
+        <a href="https://www.instagram.com/ummunadaa_" target="_blank" class="text-white" style="text-decoration: none;margin-right:0.5em">Ummu Nada</a>|
+        <a href="https://www.instagram.com/rakafebriansy" target="_blank" class="text-white" style="text-decoration: none; margin-left:0.5em;margin-right:0.5em"> Raka Febrian </a>|
+        <a href="https://www.instagram.com/juniyasyos" target="_blank" class="text-white" style="text-decoration: none;margin-left:0.5em">Ahmad Ilyas</a>
     </div>
     <!-- Right -->
 </footer>
